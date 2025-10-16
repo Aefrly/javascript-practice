758ef805fe2161f687da1452dccb36ae4df7d61b
Aefrly <fraley.allison@ic.edu>
2025-10-16 10:02:13 -0500

    Working with arrays and different methods to showcase my favorite foods.

--- script.js
+++ script.js
@@ -26,3 +26,19 @@ console.log(player.pointsPerGame); //19.2
 player.isStarter = true;
 player.numberOfGames = 254;
 console.log(player.numberOfGames); //254
+
+//Resource 2
+let emojis = ["🥦", "🍨", "🍫", "🥛"];
+console.log(emojis); //["🥦", "🍨", "🍫", "🥛"]
+
+emojis[2] = "🧀"; //Replaces 🍨 with 🧀
+console.log(emojis); //["🥦", "🧀", "🍫", "🥛"]
+
+emojis.push("🍗");
+console.log(emojis); //["🥦", "🧀", "🍫", "🥛", "🍗"]
+
+emojis.pop(); //Removes 🍗
+console.log(emojis); //["🥦", "🧀", "🍫", "🥛"]
+
+console.log(emojis.length); //4
+console.log(emojis); //["🥦", "🧀", "🍫", "🥛"]
